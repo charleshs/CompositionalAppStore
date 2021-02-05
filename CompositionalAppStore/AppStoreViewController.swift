2e270eda48b4d35fd0bf6d1c7ed2ebb8a38e1148
Charles Hsieh <charlous167@gmail.com>
2021-02-05 12:35:53 +0800

    Initial commit: create project

---
+++ CompositionalAppStore/AppStoreViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AppStoreViewController.swift
+//  CompositionalAppStore
+//
+//  Created by Charles Hsieh on 2021/2/5.
+//
+
+import UIKit
+
+final class AppStoreViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ CompositionalAppStore/BaseViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BaseViewController.swift
+//  CompositionalAppStore
+//
+//  Created by Charles Hsieh on 2021/2/5.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+    }
+}
